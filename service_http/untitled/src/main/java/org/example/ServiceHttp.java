4d8d6a5cec0456485efbb91b628f643012eb2c63
Ilyès Lounici <154410629+Lounicilyes@users.noreply.github.com>
2025-06-10 15:31:00 +0200

    Refactor fetchAPI to return String

--- service_http/untitled/src/main/java/org/example/ServiceHttp.java
+++ service_http/untitled/src/main/java/org/example/ServiceHttp.java
@@ -1,11 +1,21 @@
 package org.example;
 
-import java.net.http.HttpResponse;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+
+/**
+ * Remote service used to retrieve the body of an HTTP request.
+ */
 public interface ServiceHttp extends Remote {
 
-    HttpResponse<String> fetchAPI(String url) throws RemoteException;
+    /**
+     * Fetches the content of the given URL.
+     *
+     * @param url the address to request
+     * @return the response body as a string
+     * @throws RemoteException if a remote communication error occurs
+     */
+    String fetchAPI(String url) throws RemoteException;
 
 }
